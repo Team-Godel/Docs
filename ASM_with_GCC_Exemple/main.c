7aef5f8afda64a7f4c7fb9b7ba3e926762c426fc
Samuel Gauthier <33706474+SamGauths@users.noreply.github.com>
2017-12-15 11:57:30 -0500

    Add files via upload

---
+++ ASM_with_GCC_Exemple/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a = 10;
+    int b = 5;
+
+    __asm__("movl\t%1, %0"
+
+         : "=&r" (a) : "r" (b)
+        );
+
+        printf("a = b = %d \n", a);
+
+    return 0;
+}
